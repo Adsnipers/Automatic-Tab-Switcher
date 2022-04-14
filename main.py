c9e17e87449c8dca65e0faf7ee91a3ee858a38b8
Ashton Southall <adsnipers@outlook.com>
2022-04-14 14:00:10 +1000

    Timed tab reload

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from pynput.keyboard import Key, Controller
 import time
+keyboard = Controller()
+def main():
+    while True:
+        keyboard.press(Key.ctrl)
+        keyboard.press(Key.page_down)
+        keyboard.release(Key.ctrl)
+        keyboard.release(Key.page_down)
+        time.sleep(30)
 
-while True:
-    keyboard = Controller()
-    keyboard.press(Key.ctrl)
-    keyboard.press(Key.page_down)
-    keyboard.release(Key.ctrl)
-    keyboard.release(Key.page_down)
-    time.sleep(30)
+def timedReload():
+    while True:
+        keyboard.tap(Key.f5)
+        time.sleep(1800)
 
+main()
+timedReload()
