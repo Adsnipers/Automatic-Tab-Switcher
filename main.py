f0f99b9caff9a1ffa4149c415266b1a5831dbef9
Ashton Southall <adsnipers@outlook.com>
2022-04-19 16:13:30 +1000

    Automatically refresh current page every X iterations

--- main.py
+++ main.py
@@ -4,14 +4,20 @@ keyboard = Controller()
 
 # Timings
 tabSwitchTime = 30
+iterationsBeforeRefresh = 3
 
 def mainLoop():
     while True:
+        for i in range(iterationsBeforeRefresh):
+            keyboard.press(Key.ctrl)
+            keyboard.press(Key.page_down)
+            keyboard.release(Key.ctrl)
+            keyboard.release(Key.page_down)
+            (time.sleep(tabSwitchTime))
         keyboard.press(Key.ctrl)
-        keyboard.press(Key.page_down)
+        keyboard.press('r')
         keyboard.release(Key.ctrl)
-        keyboard.release(Key.page_down)
-        (time.sleep(tabSwitchTime))
+        keyboard.release('r')
 
 def initDash():
     print("######################")
